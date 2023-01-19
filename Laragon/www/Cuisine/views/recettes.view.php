862f2b3e4b6152cbee4590dde0801367af41be4a
LucieMeteyer <kandalucie@gmail.com>
2023-01-19 16:49:05 +0100

    MAJ 19/01/2023

--- Laragon/www/Cuisine/views/recettes.view.php
+++ Laragon/www/Cuisine/views/recettes.view.php
@@ -18,27 +18,28 @@ ob_start()
 
 </div>
 
+<div class="justify-content-center d-flex">
+    <div class="d-flex row justify-content-center m-4 gap-3 col-4">
+        <?php
 
-<div class="d-flex row justify-content-center m-4 gap-3">
-    <?php
+        for ($i = 0; $i < count($recettes); $i++) : ?>
 
-    for ($i = 0; $i < count($recettes); $i++) : ?>
+            <div class="card text-center " style=" width: 18rem; background-color: #FEE9B1; border-radius: 35px; ">
+                <img src="public/images/<?= $recettes[$i]->getImage(); ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> <?= $recettes[$i]->getNom(); ?></h5>
+                    <a href="<?= URL ?>recettes/r/<?= $recettes[$i]->getId(); ?>" style=" background-color:#B79994; color : #2F2523 ; font-family: 'Eagle Lake', cursive;" class="btn m-2 ">Voir la recette</a>
+                    <a href="<?= URL ?>recettes/m/<?= $recettes[$i]->getId(); ?>" style=" background-color:#B1C587; color : #2F2523 ; font-family: 'Eagle Lake', cursive;" class="btn ">Modifier</a>
+                    <a href="<?= URL ?>recettes/s/<?= $recettes[$i]->getId(); ?>" style=" background-color:#FF3B00; color : #2F2523 ;font-family: 'Eagle Lake', cursive;" class="btn m-2">Supprimer</a>
 
-        <div class="card col-6 text-center" style="width: 18rem; background-color: #FEE9B1; border-radius: 35px; ">
-            <img src="public/images/<?= $recettes[$i]->getImage(); ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title"> <?= $recettes[$i]->getNom(); ?></h5>
-                <a href="<?= URL ?>recettes/r/<?= $recettes[$i]->getId(); ?>" style=" background-color:#B79994; color : #2F2523 ; font-family: 'Eagle Lake', cursive;" class="btn m-2 ">Voir la recette</a>
-                <a href="<?= URL ?>recettes/m/<?= $recettes[$i]->getId(); ?>" style=" background-color:#B1C587; color : #2F2523 ; font-family: 'Eagle Lake', cursive;" class="btn ">Modifier</a>
-                <a href="<?= URL ?>recettes/s/<?= $recettes[$i]->getId(); ?>" style=" background-color:#FF3B00; color : #2F2523 ;font-family: 'Eagle Lake', cursive;" class="btn m-2">Supprimer</a>
-
+                </div>
             </div>
-        </div>
 
-    <?php endfor; ?>
+        <?php endfor; ?>
+    </div>
 </div>
 <div class="text-center p-1px ">
-    <a href="<?= URL ?>recettes/a"  style="background-color:#B79994; color : #2F2523 ;font-family: 'Eagle Lake', cursive;" class="btn d-block">Ajouter une recette</a>
+    <a href="<?= URL ?>recettes/a" style="background-color:#B79994; color : #2F2523 ;font-family: 'Eagle Lake', cursive;" class="btn d-block">Ajouter une recette</a>
 
 </div>
 
