466f2198a9d931a53180967a4b7f20e28a53d3c3
LucieMeteyer <kandalucie@gmail.com>
2023-01-25 23:05:56 +0100

    maj 25/01/2023

--- Laragon/www/Cuisine/views/afficherRecette.view.php
+++ Laragon/www/Cuisine/views/afficherRecette.view.php
@@ -7,15 +7,15 @@ ob_start()
     <h1 class="text-center m-5 p-2 ">La Cuisine de Calcifer</h1>
 </div>
 
-<div class="d-flex flex-column align-items-center m-5">
+<div class=" d-flex flex-column align-items-center m-5">
 
-    <div class="col-6 text-center" style="background-color: #FEE9B1; border-radius: 35px;">
+    <div class=" d-flex row m-3 text-center" style="background-color: #FEE9B1; border-radius: 35px;">
         <p style="font-family :'Eagle Lake', cursive;"><?= $recette->getNom() ?></p>
     </div>
-    <div class="col-6 text-center" style="background-color: #FEE9B1; border-radius: 35px;">
+    <div class="  d-flex row m-3 text-center" style="background-color: #FEE9B1; border-radius: 35px;">
         <img src="<?= URL ?>public/images/<?= $recette->getImage() ?>">
     </div>
-    <div class="col-6 text-center" style="background-color: #FEE9B1; border-radius: 35px;">
+    <div class=" d-flex row m-3 text-center" style="background-color: #FEE9B1; border-radius: 35px;">
         <p style="font-family :'Eagle Lake', cursive;">Ingrédients :</p>
         <?php foreach ($recette->getIngredients() as $ingredients) : ?>
 
@@ -23,18 +23,18 @@ ob_start()
 
         <?php endforeach ?>
     </div>
-    <div class="col-6 text-center" style="background-color: #FEE9B1; border-radius: 35px;">
-        <p style="font-family :'Eagle Lake', cursive;">Etapes :</p>
+    <div class="  d-flex flex-column m-3 text-center" style="background-color: #FEE9B1; border-radius: 35px;">
+        <p class=" text-center " style="font-family :'Eagle Lake', cursive;">Etapes :</p>
         <?php foreach ($recette->getEtapes() as $etapes) : ?>
-            <div class="col-6 text-center" style="background-color: #FEE9B1; border-radius: 35px;">
+            <div style="background-color: #FEE9B1; border-radius: 35px;">
                 <p style="font-family :'Eagle Lake', cursive;"><?= $etapes ?></p>
             </div>
         <?php endforeach ?>
     </div>
+</div>
 
 
-
-    <?php
-    $content = ob_get_clean();
-    require "template.php";
-    ?>
+<?php
+$content = ob_get_clean();
+require "template.php";
+?>
